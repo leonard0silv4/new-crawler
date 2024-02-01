7dd1b124aeaf1c603f69152c4fdfd7dbb60ecee2
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2024-02-01 14:07:35 -0300

    user logou

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,43 +1,127 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("userToken");
+    navigate("/login");
+  };
+
   return (
-    <nav className="bg-zinc-800">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="flex items-center">
-            <div className="hidden md:block">
-              <div className=" flex items-baseline space-x-4">
-                <a
-                  href="/"
-                  className="bg-zinc-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
+    <header className="bg-zinc-800">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          <a href="/" className="text-sm font-semibold leading-6 text-zinc-200">
+            Dashboard
+          </a>
+          <a
+            href="/pedidos"
+            className="text-sm font-semibold leading-6 text-zinc-200"
+          >
+            Pedidos
+          </a>
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a
+            href="#"
+            className="text-sm flex font-semibold leading-6 text-zinc-200"
+            onClick={logout}
+          >
+            Sair
+          </a>
+        </div>
+      </nav>
+      {menuOpen && (
+        <div className="lg:hidden" role="dialog" aria-modal="true">
+          <div className="fixed inset-0 z-10"></div>
+          <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+                <span className="sr-only">Your Company</span>
+              </a>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
                 >
-                  Produtos
-                </a>
-                <a
-                  href="/pedidos"
-                  className="bg-zinc-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                  aria-current="page"
-                >
-                  Pedidos
-                </a>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <a
+                    href="/"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Dashboard
+                  </a>
+                  <a
+                    href="/pedidos"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  >
+                    Pedidos
+                  </a>
+                </div>
+                <div className="py-6">
+                  <a
+                    href="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    onClick={logout}
+                  >
+                    Sair
+                  </a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="md:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-          <a
-            href="#"
-            className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-        </div>
-      </div>
-    </nav>
+      )}
+    </header>
   );
 };
 
