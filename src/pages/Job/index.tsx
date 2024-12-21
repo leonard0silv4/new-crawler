d8df5ca5bbfeb850c7ce9a0572e3bb7c76618f51
Leonardo Silva <lorsilva.fcamara@rd.com.br>
2024-12-20 22:07:49 -0300

    rename pdf

--- src/pages/Job/index.tsx
+++ src/pages/Job/index.tsx
@@ -240,7 +240,12 @@ const Job = () => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "relatorio.pdf");
+      link.setAttribute(
+        "download",
+        `${faccionist.username}_${format(new Date(), "dd_MM", {
+          locale: ptBR,
+        })}.pdf`
+      );
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
